de392506602733d18313aebcb5cee888ace73bd1
michaelcraddock <michaelcraddock@localhost>
2018-11-06 14:58:00 +0000

    Grp mtg changes 2

--- src/java/com/UserServlet.java
+++ src/java/com/UserServlet.java
@@ -7,10 +7,13 @@ package com;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import model.JDBC;
 
 /**
  *
@@ -30,22 +33,31 @@ public class UserServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
-        String user = request.getParameter("uName");
-        String password = request.getParameter("pass");
+        HttpSession session = request.getSession();
         
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet UserServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Username " + user + " password " + password + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        
+        JDBC dbBean = new JDBC();
+        dbBean.connect((Connection)request.getServletContext().getAttribute("connection"));
+        session.setAttribute("dbbean", dbBean);
+        
+        
+//        String user = request.getParameter("uName");
+//        String password = request.getParameter("pass");
+//        
+//        response.setContentType("text/html;charset=UTF-8");
+//        try (PrintWriter out = response.getWriter()) {
+//            /* TODO output your page here. You may use following sample code. */
+//            out.println("<!DOCTYPE html>");
+//            out.println("<html>");
+//            out.println("<head>");
+//            out.println("<title>Servlet UserServlet</title>");            
+//            out.println("</head>");
+//            out.println("<body>");
+//            out.println("<h1>Username " + user + " password " + password + "</h1>");
+//            out.println("</body>");
+//            out.println("</html>");
+//        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
--- src/java/model/JDBC.java
+++ src/java/model/JDBC.java
@@ -7,7 +7,11 @@ package model;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -18,6 +22,71 @@ public class JDBC {
     Statement statement = null;
     ResultSet rs = null;
     
-    //User exists
-    //user & pass exists
+    
+//    private ArrayList rsToList() throws SQLException {
+//        ArrayList aList = new ArrayList();
+//
+//        int cols = rs.getMetaData().getColumnCount();
+//        while (rs.next()) { 
+//          String[] s = new String[cols];
+//          for (int i = 1; i <= cols; i++) {
+//            s[i-1] = rs.getString(i);
+//          } 
+//          aList.add(s);
+//        } // while    
+//        return aList;
+//    } //rsToList
+// 
+//    private String makeTable(ArrayList list) {
+//        StringBuilder b = new StringBuilder();
+//        String[] row;
+//        b.append("<table border=\"3\">");
+//        for (Object s : list) {
+//          b.append("<tr>");
+//          row = (String[]) s;
+//            for (String row1 : row) {
+//                b.append("<td>");
+//                b.append(row1);
+//                b.append("</td>");
+//            }
+//          b.append("</tr>\n");
+//        } // for
+//        b.append("</table>");
+//        return b.toString();
+//    }//makeHtmlTable
+//    
+//    //User exists
+//    //user & pass exists
+//      private void select(String query){
+//        //Statement statement = null;
+//        
+//        try {
+//            statement = connection.createStatement();
+//            rs = statement.executeQuery(query);
+//            //statement.close();
+//        }
+//        catch(SQLException e) {
+//            System.out.println("way way"+e);
+//            //results = e.toString();
+//        }
+//    }
+//     public boolean exists(String user,String pass) {
+//        boolean bool = false;
+//        try  {
+//            select("select * from users where username='"+user+"'AND password='"+pass+"'");
+//            if(rs.next()) {
+//                System.out.println("TRUE");     
+//                makeTable(rsToList());
+//                bool = true;
+//            }
+//        } catch (SQLException ex) {
+//            Logger.getLogger(JDBC.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        return bool;
+//    }
+
+    public void connect(Connection connection) {
+        this.connection = connection;
+    }
+    
 }
