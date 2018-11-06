3ec62b86af47f6bd424890264490b13b7e4fb24e
michaelcraddock <michaelcraddock@localhost>
2018-11-06 14:36:37 +0000

    Grp mtg changes

---
+++ src/java/model/JDBC.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ *
+ * @author michaelcraddock
+ */
+public class JDBC {
+    Connection connection = null;
+    Statement statement = null;
+    ResultSet rs = null;
+    
+    //User exists
+    //user & pass exists
+}
