a932b30ab7264347dcdf0212e046bab47efb37c6
ryan <ryan@192.168.1.21>
2018-11-19 15:46:43 +0000

    JSP changes for cutsomer and new servlet for CreateBooking

---
+++ src/java/com/CreateBookingServlet.java
@@ -0,0 +1,144 @@
+package com;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import model.JDBC;
+
+/**
+ *
+ * @author michaelcraddock
+ */
+public class CreateBookingServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        HttpSession session = request.getSession();
+        response.setContentType("text/html;charset=UTF-8");
+
+        JDBC dbBean = (JDBC)session.getAttribute("dbBean");
+        
+        String currentAddress = request.getParameter("currentAddress");
+        String destinationAddress = request.getParameter("destinationAddress");
+        
+        System.out.println(currentAddress);
+        System.out.println(destinationAddress);
+        
+//        String errorMessage;
+//        // Check username was entered
+//        if (username.equals("")) {
+//            errorMessage = "No username Entered";
+//            request.setAttribute("errorMessage", errorMessage);
+//            request.getRequestDispatcher("/index.jsp").forward(request, response);
+//        }
+//
+//        // Check if password was entered
+//        if (password.equals("")) {
+//            errorMessage = "No password entered";
+//            request.setAttribute("errorMessage", errorMessage);
+//            request.getRequestDispatcher("/index.jsp").forward(request, response);
+//        }
+//
+//        // Check user exists
+//        if (dbBean.userExists(username)) {
+//            
+//            // Check password matches
+//            if (dbBean.passwordMatches(username, password)) {
+//                // If password matches send to welcome page
+//
+//                int user = 2;
+//
+//                try {
+//                    user = dbBean.getRole(username);
+//
+//                } catch (SQLException ex) {
+//                    Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+//                }
+//
+//                switch (user) {
+//                    case 3:
+//                        session.setAttribute("userRole", "admin");
+//                        break;
+//                    case 2:
+//                        session.setAttribute("userRole", "driver");
+//                        break;
+//                    case 1:
+//                        session.setAttribute("userRole", "customer");
+//                        break;
+//                }
+//                request.getRequestDispatcher("/welcome.jsp").forward(request, response);
+//
+//            } else {
+//                //password does not match, send to index.jsp with error message
+//                errorMessage = "Invalid password, please try again";
+//                request.setAttribute("errorMessage", errorMessage);
+//                request.getRequestDispatcher("/index.jsp").forward(request, response);
+//            }
+//
+//        } else {
+//            //user does not exist, send to index.jsp with error message
+//            errorMessage = "Invalid username, please try again";
+//            request.setAttribute("errorMessage", errorMessage);
+//            request.getRequestDispatcher("/index.jsp").forward(request, response);
+//        }
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- web/customer.jsp
+++ web/customer.jsp
@@ -5,3 +5,9 @@
 Welcome customer<br><br>
 
 <a href="">Make a booking</a>
+
+        <form method="POST" action="createBooking.do">
+              Enter current post code : <input type="text" name="currentAddress"><br> 
+              Enter destination post code : <input type="ext" name="destinationAddress"><br> 
+              <input type="submit" value="createBooking">
+        </form> 
